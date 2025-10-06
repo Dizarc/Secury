092e31f39aa249f572e3eb5103b973ca8a8fb4c7
faruk <faruk_yildiz@protonmail.com>
2025-10-06 22:19:48 +0300

    Added testing for device getting

---
+++ backend/tests/test_devices.py
@@ -0,0 +1,25 @@
+def test_get_all_devices(client):
+    response = client.get("/api/devices")
+    assert response.satus_code == 200
+
+    data = response.json()
+    assert data["success"] is True
+    assert "devices" in data
+    assert isinstance(data["devices"], list)
+    assert data["count"] == len(data["devices"])
+
+def test_get_device_valid(client):
+    response = client.get("/api/devices/1")
+    assert response.status_code == 200
+
+    data = response.json()
+    assert data["success"] is True
+    assert "device" in data
+    assert data["device"]["id"] == 1
+
+def test_get_device_invalid(client):
+    response = client.get("/api/devices/999")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["success"] is False
+    assert "error" in data
