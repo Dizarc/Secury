406c3aac47f16292eb400f1db178cc0447c9ea42
faruk <faruk_yildiz@protonmail.com>
2025-10-18 19:38:50 +0300

    Broke up the tests into different folders for better readability

--- backend/tests/api/routes/test_devices.py
+++ backend/tests/api/routes/test_devices.py
@@ -1,4 +1,4 @@
-from backend.app.models import DeviceStatus
+from backend.app.models import DeviceStatus, EventType
 
 def test_get_all_devices(client):
     response = client.get("/api/devices")
@@ -16,6 +16,7 @@ def test_get_all_devices(client):
         assert "status" in device
         assert "battery" in device
 
+
 def test_get_device_valid(client):
     response = client.get("/api/devices/1")
     assert response.status_code == 200
@@ -31,9 +32,36 @@ def test_get_device_valid(client):
     assert "last_updated" in data
     assert "last_seen" in data
 
+
 def test_get_device_invalid(client):
     response = client.get("/api/devices/999")
     assert response.status_code == 404
     
     data = response.json()
+    assert data["detail"] == "Device not found"
+
+
+def test_trigger_valid_device(client):
+    response = client.get("/api/devices/1/trigger?new_status=open")
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["success"] is True
+    assert "device" in data
+    assert "event" in data
+
+    assert data["device"]["status"] == DeviceStatus.OPEN.value
+    assert data["device"]["id"] == 1
+
+    assert data["event"]["type"] == EventType.STATUS_CHANGE.value
+    assert data["event"]["device_id"] == 1
+
+
+def  test_trigger_invalid_device(client):
+    response = client.get("/api/devices/999/trigger?new_status=open")
+    assert response.status_code == 404
+
+    data = response.json()
+    
     assert data["detail"] == "Device not found"
